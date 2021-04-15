538269b26b325de973e0fb3e0c6589bc4ef4da27
Balanced_being <viengooleg@gmail.com>
2021-04-15 13:49:59 +0300

    fix data order

--- src/components/order.vue
+++ src/components/order.vue
@@ -4,131 +4,119 @@
       <h3>ФИО</h3>
       <div
         class="form-group"
-        :class="{ 'form-group--error': $v.orderData.surname.value.$error }"
+        :class="{ 'form-group--error': $v.orderData.surname.$error }"
       >
         <input
           class="order__forms-input"
           type="text"
-          v-model="$v.orderData.surname.value.$model"
-          :placeholder="orderData.surname.placeholder"
-          :id="orderData.surname.id"
+          v-model="$v.orderData.surname.$model"
+          placeholder="Введите фамилию"
         />
         <div
           class="error"
-          v-if="
-            !$v.orderData.surname.value.required &&
-            $v.orderData.surname.value.$dirty"
+          v-if="!$v.orderData.surname.required && $v.orderData.surname.$dirty"
         >
           Поле, обязательное для заполнения
         </div>
         <div
           class="error"
-          v-if="
-            !$v.orderData.surname.value.minLength && orderData.surname.value"
+          v-if="!$v.orderData.surname.minLength && orderData.surname"
         >
           Имя должно иметь не менее
-          {{ $v.orderData.surname.value.$params.minLength.min }} символа.
+          {{ $v.orderData.surname.$params.minLength.min }} символа.
         </div>
       </div>
       <div
         class="form-group"
-        :class="{ 'form-group--error': $v.orderData.name.value.$error }"
+        :class="{ 'form-group--error': $v.orderData.name.$error }"
       >
         <input
           class="order__forms-input"
           type="text"
-          v-model="$v.orderData.name.value.$model"
-          :placeholder="orderData.name.placeholder"
-          :id="orderData.name.id"
+          v-model="$v.orderData.name.$model"
+          placeholder="Введите имя"
         />
         <div
           class="error"
-          v-if="
-            !$v.orderData.name.value.required && $v.orderData.name.value.$dirty"
+          v-if="!$v.orderData.name.required && $v.orderData.name.$dirty"
         >
           Поле, обязательное для заполнения
         </div>
         <div
           class="error"
-          v-if="!$v.orderData.name.value.minLength && orderData.name.value"
+          v-if="!$v.orderData.name.minLength && orderData.name"
         >
           Имя должно иметь не менее
-          {{ $v.orderData.name.value.$params.minLength.min }} символа.
+          {{ $v.orderData.name.$params.minLength.min }} символа.
         </div>
       </div>
       <div
         class="form-group"
-        :class="{ 'form-group--error': $v.orderData.patronymic.value.$error }"
+        :class="{ 'form-group--error': $v.orderData.patronymic.$error }"
       >
         <input
           class="order__forms-input"
           type="text"
-          v-model="$v.orderData.patronymic.value.$model"
-          :placeholder="orderData.patronymic.placeholder"
-          :id="orderData.patronymic.id"
+          v-model="$v.orderData.patronymic.$model"
+          placeholder="Отчество"
         />
         <div
           class="error"
           v-if="
-            !$v.orderData.patronymic.value.required &&
-            $v.orderData.patronymic.value.$dirty"
+            !$v.orderData.patronymic.required && $v.orderData.patronymic.$dirty
+          "
         >
           Поле, обязательное для заполнения
         </div>
         <div
           class="error"
-          v-if="
-            !$v.orderData.patronymic.value.minLength &&
-            orderData.patronymic.value"
+          v-if="!$v.orderData.patronymic.minLength && orderData.patronymic"
         >
           Имя должно иметь не менее
-          {{ $v.orderData.patronymic.value.$params.minLength.min }} символа.
+          {{ $v.orderData.patronymic.$params.minLength.min }} символа.
         </div>
       </div>
     </div>
     <div
       class="form-group"
-      :class="{ 'form-group--error': $v.orderData.address.value.$error }"
+      :class="{ 'form-group--error': $v.orderData.address.$error }"
     >
       <input
         class="order__forms-input"
         type="text"
-        v-model="$v.orderData.address.value.$model"
-        :placeholder="orderData.address.placeholder"
-        :id="orderData.address.id"
+        v-model="$v.orderData.address.$model"
+        placeholder="address vvedite"
       />
       <div
         class="error"
-        v-if="
-          !$v.orderData.address.value.required &&
-          $v.orderData.address.value.$dirty"
+        v-if="!$v.orderData.address.required && $v.orderData.address.$dirty"
       >
         Поле, обязательное для заполнения
       </div>
       <div
         class="error"
-        v-if="!$v.orderData.address.value.minLength && orderData.address.value"
+        v-if="!$v.orderData.address.minLength && orderData.address"
       >
         Имя должно иметь не менее
-        {{ $v.orderData.address.value.$params.minLength.min }} символа.
+        {{ $v.orderData.address.$params.minLength.min }} символа.
       </div>
     </div>
     <div>
-      <label v-for="ways in orderData.way.addressItems" :key="ways.id">
+      <label v-for="ways in deliveryTypes" :key="ways.id">
         <input
           name="ways"
           type="radio"
           :value="ways.value"
           :id="ways.id"
-          v-model="orderData.way.checked"
+          v-model="orderData.way"
         />
         {{ ways.title }}
       </label>
     </div>
-    <div v-if="this.orderData.way.checked == 'NewMail'">
-      <select class="order__forms-select">
+    <div v-if="this.orderData.way == 'NewMail'">
+      <select class="order__forms-select" v-model="orderData.newMail">
         <option
-          v-for="newMail in orderData.newMail.addressItems"
+          v-for="newMail in newPostDepartments"
           :value="newMail.value"
           :key="newMail.id"
           :id="newMail.id"
@@ -137,10 +125,10 @@
         </option>
       </select>
     </div>
-    <div v-if="this.orderData.way.checked == 'UkrMail'">
-      <select class="order__forms-select">
+    <div v-if="this.orderData.way == 'UkrMail'">
+      <select class="order__forms-select" v-model="orderData.UkrMail">
         <option
-          v-for="ukrMail in orderData.ukrMail.addressItems"
+          v-for="ukrMail in UkrMailDepartments"
           :value="ukrMail.value"
           :key="ukrMail.id"
           :id="ukrMail.id"
@@ -149,30 +137,33 @@
         </option>
       </select>
     </div>
-    <div v-if="this.orderData.way.checked == 'Pickup'" :class="{ 'form-group--error': $v.orderData.pickup.value.$error }">
+    <div
+      v-if="this.orderData.way == 'orderDescription'"
+      :class="{ 'form-group--error': $v.orderData.orderDescription.$error }"
+    >
       <textarea
-        v-model="$v.orderData.pickup.value.$model"
-        :placeholder="orderData.pickup.placeholder"
-        :id="orderData.pickup.id"
+        v-model="$v.orderData.orderDescription.$model"
+        placeholder="Напишите комментарий"
       >
       </textarea>
       <div
         class="error"
         v-if="
-          !$v.orderData.pickup.value.required &&
-          $v.orderData.pickup.value.$dirty"
+          !$v.orderData.orderDescription.required &&
+          $v.orderData.orderDescription.$dirty"
       >
         Поле, обязательное для заполнения
       </div>
       <div
         class="error"
-        v-if="!$v.orderData.pickup.value.minLength && orderData.pickup.value"
+        v-if="
+          !$v.orderData.orderDescription.minLength && orderData.orderDescription"
       >
         Имя должно иметь не менее
-        {{ $v.orderData.pickup.value.$params.minLength.min }} символа.
+        {{ $v.orderData.orderDescription.$params.minLength.min }} символа.
       </div>
     </div>
-    <button :click="sendForms" class="order__forms-btn">Отправить</button>
+    <button @click="sendForms" class="order__forms-btn">Отправить</button>
   </div>
 </template>
 
@@ -182,135 +173,121 @@ export default {
   name: "order",
   data: function () {
     return {
-      orderAddress: '',
+      deliveryTypes: [
+        {
+          id: 5,
+          value: "NewMail",
+          title: "Новая Почта",
+        },
+        {
+          id: 6,
+          value: "UkrMail",
+          title: "Укр Почта",
+        },
+        {
+          id: 7,
+          value: "orderDescription",
+          title: "Самовывоз",
+        },
+      ],
+      newPostDepartments: [
+        {
+          id: 8,
+          value: "Poltava",
+          title: "Poltava",
+        },
+        {
+          id: 9,
+          value: "Nikolaev",
+          title: "Nikolaev",
+        },
+        {
+          id: 10,
+          value: "Odessa",
+          title: "Odessa",
+        },
+      ],
+      UkrMailDepartments: [
+        {
+          id: 8,
+          value: "Poltava-1",
+          title: "Poltava-2",
+        },
+        {
+          id: 9,
+          value: "Nikolaev-2",
+          title: "Nikolaev-2",
+        },
+        {
+          id: 10,
+          value: "Odessa-3",
+          title: "Odessa-3",
+        },
+      ],
       orderData: {
-        surname: {
-          id: 1,
-          value: "",
-          placeholder: "Фамилия",
-          title: "Фамилия",
-        },
-        name: {
-          id: 2,
-          value: "",
-          placeholder: "Имя",
-        },
-        patronymic: {
-          id: 3,
-          value: "",
-          placeholder: "Отчество",
-        },
-        address: {
-          id: 4,
-          value: "",
-          placeholder: "Адрес",
-        },
-        way: {
-          checked: "NewMail",
-          addressItems: [
-            {
-              id: 5,
-              value: "NewMail",
-              title: "Новая Почта",
-            },
-            {
-              id: 6,
-              value: "UkrMail",
-              title: "Укр Почта",
-            },
-            {
-              id: 7,
-              value: "Pickup",
-              title: "Самовывоз",
-            },
-          ],
-        },
-        newMail: {
-          checked: "Poltava",
-          addressItems: [
-            {
-              id: 8,
-              value: "Poltava",
-              title: "Poltava",
-            },
-            {
-              id: 9,
-              value: "Nikolaev",
-              title: "Nikolaev",
-            },
-            {
-              id: 10,
-              value: "Odessa",
-              title: "Odessa",
-            },
-          ],
-        },
-        ukrMail: {
-          checked: "Poltava-1",
-          addressItems: [
-            {
-              id: 8,
-              value: "Poltava-1",
-              title: "Poltava-2",
-            },
-            {
-              id: 9,
-              value: "Nikolaev-2",
-              title: "Nikolaev-2",
-            },
-            {
-              id: 10,
-              value: "Odessa-3",
-              title: "Odessa-3",
-            },
-          ],
-        },
-        pickup: {
-          id: 11,
-          value: "",
-          placeholder: "Введите ваши данные",
-        },
+        surname: "",
+        name: "",
+        patronymic: "",
+        address: "",
+        way: "UkrMail",
+        newMail: "",
+        ukrMail: "",
+        orderDescription: "",
       },
     };
   },
   validations: {
     orderData: {
       surname: {
-        value: {
-          required,
-          minLength: minLength(6),
-        },
+        required,
+        minLength: minLength(6),
       },
       name: {
-        value: {
-          required,
-          minLength: minLength(6),
-        },
+        required,
+        minLength: minLength(6),
       },
       patronymic: {
-        value: {
-          required,
-          minLength: minLength(6),
-        },
+        required,
+        minLength: minLength(6),
       },
       address: {
-        value: {
-          required,
-          minLength: minLength(6),
-        },
+        required,
+        minLength: minLength(6),
       },
-      pickup: {
-        value: {
-          required,
-          minLength: minLength(8),
-        },
+      orderDescription: {
+        required,
+        minLength: minLength(8),
       },
     },
   },
   methods: {
     sendForms: function () {
-      console.log(this.orderAddress)
-    }
+      //   /*     this.axios.get("http//").then((response) => {
+      //   this.orderData = response.data;
+      //   console.log(this.orderData);
+      // }); */
+      this.axios
+        .post("/asdasdasd", {
+          firstName: "Fred",
+          lastName: "Flintstone",
+          orderData: {
+            surname: "",
+            name: this.name.data,
+            patronymic: "",
+            address: "",
+            way: "",
+            newMail: "",
+            ukrMail: "",
+            orderDescription: "",
+          }
+        })
+        .then(function (response) {
+          console.log(response);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    },
   },
   watch: {
     orderData: {
