3712346a6b829f0727a98cea9c3c68c89e71d099
Balanced_being <viengooleg@gmail.com>
2021-04-29 15:55:52 +0300

    добавил переделанный сайт kidsanna

--- src/components/kidsanna.vue
+++ src/components/kidsanna.vue
@@ -3,35 +3,36 @@
         <div class="kidsanna">
             <div class="container">
                 <div class="kidsanna__wrapper">
-                    <VueSlickCarousel class="carusel__sneakers" v-bind="settings">
-                        <div class="carusel__sneakers-item" v-for="sneakers in colorData.sneakers" :key="sneakers.id" :id="sneakers.id">
-                            <img class="carusel__sneakers--logo" :src="sneakers.img" alt="">
+                    <VueSlickCarousel class="carusel__items" v-bind="settings">
+                        <div class="carusel__item" v-for="productItem in colorData.productItems" :key="productItem.id">
+                            <img class="carusel__item-logo" :src="productItem.img" alt="">
                         </div>
                     </VueSlickCarousel>
-                    <div class="presence">
-                        <div class="presence__head">
-                            <div class="presence__head-instock" :class="colorData.presenceAvailable.class">{{colorData.presenceAvailable.title}}</div>
-                            <div class="presence__head-vendorcode">{{colorData.presenceVendorCode}}</div>
+                    <div class="info">
+                        <div class="info__head">
+                            <div class="info__head-instock" :class="{'not-in-stock': colorData.status !== 'В наличии' }">{{colorData.status}}</div>
+                            <div class="info__head-vendorcode">Артикул: {{colorData.vendorCode}}</div>
                         </div>
-                        <div class="presence__price">
-                            <p class="presence__price-text--strike"><strike>{{colorData.priceProduct.value}}</strike><sub>{{colorData.priceProduct.productСurrency}}</sub></p>
-                            <p class="presence__price-text">{{colorData.productPriceDiscount.value}}<sub>{{colorData.productPriceDiscount.productСurrency}}</sub></p>
+                        <div class="price">
+                            <p class="price__text--strike"><strike>{{colorData.priceProduct.value}}</strike><sub>{{colorData.priceProduct.currency}}</sub></p>
+                            <p class="price__text">{{colorData.productPriceDiscount.value}}<sub>{{colorData.productPriceDiscount.currency}}</sub></p>
                         </div>
-                        <div class="presence__offer">
-                            <h3 class="presence__offer-title">Цвет:</h3>
+                        <div class="info__offer">
+                            <h3 class="info__offer-title">Цвет:</h3>
                             <label v-for="colors in viewsColor" :key="colors.id">
                                 <input
-                                :class="colors.class"
+                                class="colors__select"
                                 type="radio"
-                                :id="colors.id"
                                 name="age"
+                                :id="colors.id"
                                 v-model="selectedСolor"
                                 :value="colors.value"
+                                @change="request(colors.id)"
                                 />
-                                <span :class="colors.class" class="colors"></span>
+                                <span class="colors__select" :style="{ backgroundColor: colors.color }"></span>
                             </label>
                         </div>
-                        <button :class="colorData.kidsannaColorClass" class="kidsanna__product-buy">Купить</button>
+                        <button class="kidsanna__buy">Купить</button>
                     </div>
                 </div>
             </div>
@@ -59,50 +60,30 @@ export default {
             {
             id: 1,
             value: "ColorRed",
-            class: "presence__colors-red presence__colors"
+            color: '#DD292E'
             },
             {
             id: 2,
             value: "ColorGreen",
-            class: "presence__colors-green presence__colors"
+            color: '#2BA401'
             },
             {
             id: 3,
             value: "ColorOrange",
-            class: "presence__colors-orange presence__colors"
+            color: '#FFB13C'
             },
         ],
         colorData: {},
     };
   },
-  watch: {
-    selectedСolor: {
-      handler: function (val) {
-        if (val == 'ColorRed') {
-            console.log(val)
-            this.axios.get("./static/color1.json").then((response) => {
-            this.colorData = response.data;
-            this.pending = false;
-            console.log(this.colorData);
+  methods: {
+    request: function(colorId){
+            this.axios.get("./static/color"+colorId+".json")
+            .then((response) => {
+                this.colorData = response.data;
+                this.pending = false;
             });
-        } else if (val == 'ColorGreen') {
-            console.log(val)
-            this.axios.get("./static/color2.json").then((response) => {
-            this.colorData = response.data;
-            this.pending = false;
-            console.log(this.colorData);
-            });
-        } else if (val == 'ColorOrange') {
-            console.log(val)
-            this.axios.get("./static/color3.json").then((response) => {
-            this.colorData = response.data;
-            this.pending = false;
-            console.log(this.colorData);
-            });
-        }
-      },
-      deep: true,
-    },
+    }
   },
   created: function () {
     this.axios.get("./static/color1.json").then((response) => {
@@ -115,37 +96,47 @@ export default {
 </script>
 
 <style scoped lang='scss'>
+@import url('https://fonts.googleapis.com/css2?family=Balsamiq+Sans:wght@700&family=Roboto:wght@400;500&display=swap');
 body {
     background: #F5F5F5;
 }
+
+.container {
+    max-width: 1260px;
+    margin: 0 auto;
+}
+
+.carusel__items {
+    width: 500px;
+    margin-right: 30px;
+    .carusel__item {
+        height: 500px;
+        &-logo {
+            object-fit: cover;
+            height: 500px;
+        }
+    }    
+}
+
 input[type="radio"] {
     display: none;
     
 }
-.presence__colors {
+.colors__select {
     width: 30px;
     height: 30px;
     border-radius: 50%;
     display: inline-block;
     box-sizing: border-box;
     position: relative;
-    margin-left: 5px;
     cursor: pointer;
+    margin-right: 5px;
     &:checked + &,
     &:checked + &,
     &:checked + & {
         width: 30px;
         height: 30px;
-        border: 2px solid #aaaaaa;
-    }
-    &-red {
-        background-color: red;
-    }
-    &-green {
-        background-color: green;
-    }
-    &-orange {
-        background-color: orange;
+        border: 2px solid #F5F5F5;
     }
     &::before {
         content: '';
@@ -158,35 +149,23 @@ input[type="radio"] {
         opacity: 0;
         transition: all .2s;
     }
-    &-red:checked + &::before,
-    &-green:checked + &::before,
-    &-orange:checked + &::before {
+    &:checked + &::before {
         opacity: 1;
     }
 }
 
-.presence__offer {
-    display: block;
-    text-align: left;
-    &-title {
-        font-weight: 500;
-        font-size: 14px;
-        line-height: 120%;
-        color: #4F4F4F;
-    }
-}
+
 .kidsanna {
     max-width: 1260px;
     margin: 0 auto;
+    font-family: 'Roboto', sans-serif;
     &__wrapper {
         display: flex;
     }
-    &__product-buy {
+    &__buy {
         display: block;
         width: 210px;
         height: 50px;
-        margin: 20px auto 0;
-        border: none;
         border-radius: 30px;
         font-weight: bold;
         font-size: 14px;
@@ -195,13 +174,11 @@ input[type="radio"] {
         color: #FFFFFF;
         transition: all .2s;
         cursor: pointer;
-    }
-
-    &__color-red {
         background: red;
         border: 1px solid transparent;
         position: relative;
         padding-left: 20px;
+        box-sizing: border-box;
         &::before {
             content: '';
             position: absolute;
@@ -211,85 +188,12 @@ input[type="radio"] {
             left: 60px;
             top: 14px;
         }
-        &:hover::before {
-            background-image: url('../../src/assets/images/basket-red.svg');
-        }
-        &:hover {
-            background-color: #fff;
-            border: 1px solid red;
-            color: red;
-        }
-    }
-
-    &__color-green {
-        background: green;
-        border: 1px solid transparent;
-        position: relative;
-        padding-left: 20px;
-        &::before {
-            content: '';
-            position: absolute;
-            background-image: url('../../src/assets/images/basket.svg');
-            width: 16px;
-            height: 16px;
-            left: 60px;
-            top: 14px;
-        }
-        &:hover::before {
-            background-image: url('../../src/assets/images/basket-green.svg');
-        }
-        &:hover {
-            background-color: #fff;
-            border: 1px solid green;
-            color: green;
-        }
-    }
-
-    &__color-orange {
-        background: orange;
-        border: 1px solid transparent;
-        position: relative;
-        padding-left: 20px;
-        &::before {
-            content: '';
-            position: absolute;
-            background-image: url('../../src/assets/images/basket.svg');
-            width: 16px;
-            height: 16px;
-            left: 60px;
-            top: 14px;
-        }
-        &:hover::before {
-            background-image: url('../../src/assets/images/basket-orange.svg');
-        }
-        &:hover {
-            background-color: #fff;
-            border: 1px solid orange;
-            color: orange;
-        }
     }
 }
 
-.container {
-    max-width: 1260px;
-    margin: 0 auto;
-}
-
-.carusel__sneakers {
-    width: 504px;
-    height: 482px;
-    margin-right: 20px;
-    &-item {
-        width: 504px;
-        height: 482px;
-    }
-    &--logo {
-        object-fit: cover;
-        height: 500px;
-    }
-}
-.presence {
-    width: 50%;
+.info {
+    max-width: 600px;
+    width: 100%;
     &__head {
     display: flex;
     justify-content: space-between;
@@ -301,65 +205,116 @@ input[type="radio"] {
         font-weight: normal;
         font-size: 14px;
         line-height: 120%;
-        border: 1px solid #969595;
+        border: 1px solid #F5F5F5;
         box-sizing: border-box;
         border-radius: 20px;
         display: flex;
         justify-content: center;
-        align-items: center;      
-    } 
-    &__head--available {
+        align-items: center; 
         color: #00930F;
-    }
-    &__head--not-available {
-        color: #930500;
-    }
+        &.not-in-stock {
+            color: #F04137;
+        }     
+    } 
     &__head-vendorcode {
         font-size: 14px;
         line-height: 120%;
         color: #595959;     
     } 
-    &__price {
-        font-weight: bold;
-        font-size: 30px;
-        line-height: 100%;
-        text-transform: uppercase;
-        display: flex;
-        justify-content: center;
-        
-    }
-    &__price-text {    
-        color: #F04137;
-        sub {
-            font-size: 12px;
-        }
-            &--strike {
-            color: #828282;
-            margin-right: 20px;
-            sub {
-                font-size: 12px;
-            }
-        }
-    }
+    
     &__colors-red,
     &__colors-green,
     &__colors-orange {
         width: 30px;
         height: 30px;
     }
+
+    &__offer {
+        display: block;
+        text-align: left;
+        margin-bottom: 40px;
+        &-title {
+            font-weight: 500;
+            font-size: 14px;
+            line-height: 120%;
+            color: #4F4F4F;
+            margin: 0 0 20px 0;
+        }
+    }
 }
 
-.slick-prev:before,
+.price {
+    font-weight: bold;
+    font-size: 30px;
+    line-height: 100%;
+    text-transform: uppercase;
+    display: flex;
+    justify-content: center;
+    font-family: 'Balsamiq Sans', cursive;
+    font-weight: bold;
+    align-items: center;
+    justify-content: flex-start;
+    .price__text {    
+        color: #F04137;
+        font-weight: bold;
+        font-size: 30px;
+        line-height: 100%;
+        text-transform: uppercase;
+        margin: 30px 0;
+        sub {
+            font-size: 14px;
+            line-height: 100%;
+            font-family: 'Roboto', sans-serif;
+        }
+        &--strike {
+            color: #828282;
+            margin-right: 20px;
+            font-weight: bold;
+            font-size: 20px;
+            line-height: 100%;
+            text-transform: uppercase;
+            color: #828282;
+            sub {
+                font-size: 14px;
+                line-height: 100%;
+                font-family: 'Roboto', sans-serif;
+            }
+        }
+    }
+}
+
+
+
+</style>
+<style lang="scss">
 .slick-next:before {
-  font-family: 'slick';
-  font-size: 20px;
-  line-height: 1;
-
-  opacity: 0.75;
-  color: rgb(0, 0, 0) !important;
-
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+    width: 31px;
+    height: 32px;
+    display: block;
+    content: '';
+    background-image: url('../../src/assets/images/arrow-right.svg');
+    font-family: 'Roboto', sans-serif;
+    background: #F5F5F5;
+    opacity: 0.5;
+    border-radius: 20px 0px 0px 20px;
+}
+.slick-prev::before {
+    width: 31px;
+    height: 32px;
+    display: block;
+    content: '';
+    background-image: url('../../src/assets/images/arrow-right.svg');
+    font-family: 'Roboto', sans-serif;
+    background: #F5F5F5;
+    opacity: 0.5;
+    border-radius: 0px 20px 20px 0px;
+}
+.slick-next {
+    right: 10px;
 }
 
+.slick-prev {
+    left: 0px;
+    z-index: 1;
+}
 </style>
